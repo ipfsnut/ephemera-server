f95a8a3324949a87eca2d2f1697429fbcb9d704a
Dylan <40326908+ipfsnut@users.noreply.github.com>
2024-04-29 20:37:23 -0500

    Update ipfs.js
    now we're cookin'.

--- utils/ipfs.js
+++ utils/ipfs.js
@@ -1,23 +1,53 @@
-const create = require('ipfs-http-client');
-const ipfs = create('http://127.0.0.1:5001');
+const axios = require('axios');
+
+const ipfsApiUrl = 'http://165.22.184.42:5001/api/v0';
 
 const addFileToIPFS = async (fileBuffer) => {
-  const fileAdded = await ipfs.add(fileBuffer);
-  const cid = fileAdded.cid.toString();
-  return cid;
+  try {
+    const formData = new FormData();
+    formData.append('file', fileBuffer);
+
+    const response = await axios.post(`${ipfsApiUrl}/add`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    const cid = response.data.Hash;
+    return cid;
+  } catch (error) {
+    console.error('Error adding file to IPFS:', error);
+    throw error;
+  }
 };
 
 const pinFileToIPFS = async (cid) => {
-  await ipfs.pin.add(cid);
+  try {
+    await axios.post(`${ipfsApiUrl}/pin/add?arg=${cid}`);
+  } catch (error) {
+    console.error('Error pinning file to IPFS:', error);
+    throw error;
+  }
 };
 
 const getPinnedFiles = async () => {
-  const pinnedFiles = await ipfs.pin.ls();
-  return pinnedFiles.map((file) => file.cid.toString());
+  try {
+    const response = await axios.post(`${ipfsApiUrl}/pin/ls`);
+    const pinnedFiles = response.data.Keys;
+    return pinnedFiles;
+  } catch (error) {
+    console.error('Error retrieving pinned files:', error);
+    throw error;
+  }
 };
 
 const unpinFile = async (cid) => {
-  await ipfs.pin.rm(cid);
+  try {
+    await axios.post(`${ipfsApiUrl}/pin/rm?arg=${cid}`);
+  } catch (error) {
+    console.error('Error unpinning file from IPFS:', error);
+    throw error;
+  }
 };
 
 module.exports = {
@@ -25,5 +55,4 @@ module.exports = {
   pinFileToIPFS,
   getPinnedFiles,
   unpinFile,
-  ipfs,
 };
